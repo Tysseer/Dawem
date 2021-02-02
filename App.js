9bf25cfca34f0e57c0f6ae4d14bde1d849120b81
Tysseer <Tysseer.Harak@gmail.com>
2021-02-02 12:51:35 -0500

    navigation first step

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { StyleSheet } from "react-native";
-import ScreenQuranBrowser from "./app/js/screens/ScreenQuranBrowser";
 import QuranReader from "./app/js/helpers/QuranReader";
-//import RevisionsManager from "./app/js/helpers/RevisionsManager";
-//import ScreenRevisions from "./app/js/screens/ScreenRevisions";
-//import ScreenWelcome from "./app/js/screens/ScreenWelcome";
-//import ScreenLanguage from "./app/js/screens/ScreenLanguage";
+import RevisionsManager from "./app/js/helpers/RevisionsManager";
+import ScreenRevisions from "./app/js/screens/ScreenRevisions";
+import ScreenWelcome from "./app/js/screens/ScreenWelcome";
+import ScreenLanguage from "./app/js/screens/ScreenLanguage";
+import ScreenQuranBrowser from "./app/js/screens/ScreenQuranBrowser";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default class App extends Component {
   constructor(props) {
@@ -13,9 +16,20 @@ export default class App extends Component {
   }
 
   render() {
-    var read = new QuranReader();
-    var page = read.getPage(3);
-    return <ScreenQuranBrowser strtPage={page} quranReader={read} />;
+    const Stack = createStackNavigator();
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ScrWelcome" component={ScreenWelcome} />
+          <Stack.Screen name="SctLang" component={ScreenLanguage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+
+    // var read = new QuranReader();
+    // var page = read.getPage(3);
+    // return <ScreenQuranBrowser strtPage={page} quranReader={read} />;
     //return <ScreenRevisions revisionsManager={rev} />;
     //return <ScreenWelcome />;
     //return <ScreenLanguage />;
