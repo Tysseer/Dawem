9953754d58a98938af6a934a5678c3cf1c815437
Tysseer <Tysseer.Harak@gmail.com>
2021-02-02 07:07:16 -0500

    made the app a class, quranreader ready for nav

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet } from "react-native";
 import ScreenQuranBrowser from "./app/js/screens/ScreenQuranBrowser";
 import QuranReader from "./app/js/helpers/QuranReader";
@@ -7,15 +7,20 @@ import QuranReader from "./app/js/helpers/QuranReader";
 //import ScreenWelcome from "./app/js/screens/ScreenWelcome";
 //import ScreenLanguage from "./app/js/screens/ScreenLanguage";
 
-export default function App() {
-  var read = new QuranReader();
-  var page = read.getPage(3);
-  return <ScreenQuranBrowser strtPage={page} quranReader={read} />;
-  //return <ScreenRevisions revisionsManager={rev} />;
-  //return <ScreenWelcome />;
-  //return <ScreenLanguage />;
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    var read = new QuranReader();
+    var page = read.getPage(3);
+    return <ScreenQuranBrowser strtPage={page} quranReader={read} />;
+    //return <ScreenRevisions revisionsManager={rev} />;
+    //return <ScreenWelcome />;
+    //return <ScreenLanguage />;
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
