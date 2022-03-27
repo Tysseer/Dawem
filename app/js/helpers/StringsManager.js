5df6e4f6bf7a0538a62f600cf0a7f28a6c9f2c0e
kareem aly <kareemaly@kareems-MacBook-Pro.local>
2022-03-27 11:10:29 +0300

    add add and cancel

--- app/js/helpers/StringsManager.js
+++ app/js/helpers/StringsManager.js
@@ -26,6 +26,7 @@ export const STR_REVISED = 25;
 export const STR_SEL_LANGUAGE = 26;
 export const STR_REV_TITLE = 27;
 export const STR_ADD_REV = 28;
+export const STR_CANSEL = 29;
 export default class StringsManager {
   constructor() {
     this.strLang = "ar";
@@ -46,6 +47,11 @@ export default class StringsManager {
     if (nStrID == STR_ADD_REV) {
       if (strLang == "ar") return "+ اضف";
       if (strLang == "en") return "+ Add";
+      return "unsupported language";
+    }
+    if (nStrID == STR_CANSEL) {
+      if (strLang == "ar") return "إلغاء";
+      if (strLang == "en") return "+ Cansel";
       return "unsupported language";
     }
     if (nStrID == STR_SEL_AYAH) {
