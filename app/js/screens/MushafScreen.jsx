573b0cf4aa0b771998183ee9c3b810986ce6453e
kareem aly <kareemaly@kareems-MacBook-Pro.local>
2022-03-31 07:38:56 +0300

    MushafScreen v2

--- app/js/screens/MushafScreen.jsx
+++ app/js/screens/MushafScreen.jsx
@@ -1,19 +1,19 @@
-import React, { useCallback, useEffect } from 'react';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
+import React, { useCallback, useEffect } from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 
-import Screen from 'app/components/Screen';
-import QuranReaderByLine from 'app/js/helpers/QuranReaderByLine';
-import { colors } from '../../constants';
-import SurahHeader from '../../assets/svg/SurahHeader';
-import Doaa from 'assets/svg/Doaa';
-import allQuranLines from '../helpers/quranLines';
-import SVGLoader from '../helpers/SVGLoader';
-import Center from '../../components/Center';
-import TestComp from 'app/components/TestComp';
-import AdjustLabel from '../../components/Adjustable';
+import Screen from "app/components/Screen";
+import QuranReaderByLine from "app/js/helpers/QuranReaderByLine";
+import { colors } from "../../constants";
+import SurahHeader from "../../assets/svg/SurahHeader";
+import Doaa from "assets/svg/Doaa";
+import allQuranLines from "../helpers/quranLines";
+import SVGLoader from "../helpers/SVGLoader";
+import Center from "../../components/Center";
+import TestComp from "app/components/TestComp";
+import AdjustLabel from "../../components/Adjustable";
 
-const { width } = Dimensions.get('window');
+const { width } = Dimensions.get("window");
 const MushafScreen = () => {
   const quranReader = new QuranReaderByLine();
   const svgLoader = new SVGLoader();
@@ -23,7 +23,7 @@ const MushafScreen = () => {
   const renderSurahHeader = (name) => (
     <View
       key={Math.random().toString()}
-      style={{ position: 'relative', marginVertical: 20 }}
+      style={{ position: "relative", marginVertical: 20 }}
     >
       <View style={styles.surahHeader}>
         <Text style={{}}>{name}</Text>
@@ -35,7 +35,15 @@ const MushafScreen = () => {
   const Basmalah = () => (
     <Text
       key={Math.random().toString()}
-      style={{ textAlign: 'center', marginBottom: 10 }}
+      // style={{ textAlign: "center", marginBottom: 10 }}
+      style={{
+        // backgroundColor: "#ff0",
+        fontSize: width * 0.043,
+        // flexGrow: 1,
+        textAlign: "center",
+        // textAlign: "justify",
+        // letterSpacing: 3.3,
+      }}
     >
       بسم الله الرحمن الرحيم
     </Text>
@@ -44,20 +52,20 @@ const MushafScreen = () => {
   const renderAyat2 = (ayat) => (
     <Text
       style={{
-        backgroundColor: 'green',
-        flexDirection: 'row',
-        justifyContent: 'center',
+        backgroundColor: "green",
+        flexDirection: "row",
+        justifyContent: "center",
       }}
     >
       {ayat.map((ayah) => (
         <Text
           key={Math.random().toString()}
           style={{
-            textAlign: 'left',
+            textAlign: "left",
             // alignSelf: 'stretch',
-            backgroundColor: '#ff0',
+            backgroundColor: "#ff0",
             // justifyContent: 'center',
-            width: '100%',
+            width: "100%",
           }}
         >
           {ayah.txt}
@@ -66,8 +74,8 @@ const MushafScreen = () => {
             <Text
               style={{
                 width: 50,
-                backgroundColor: 'red',
-                textAlign: 'center',
+                backgroundColor: "red",
+                textAlign: "center",
               }}
             >
               {ayah.num}
@@ -84,8 +92,8 @@ const MushafScreen = () => {
         <View
           key={Math.random().toString()}
           style={{
-            flexDirection: 'row',
-            backgroundColor: '#f0f',
+            flexDirection: "row",
+            backgroundColor: "#f0f",
 
             // alignSelf: 'baseline',
             // width: '100%',
@@ -95,11 +103,11 @@ const MushafScreen = () => {
           {/* {<AdjustLabel text={ayah.txt} numberOfLines={1} fontSize={21} />} */}
           <Text
             style={{
-              backgroundColor: '#ff0',
+              backgroundColor: "#ff0",
               fontSize: width * 0.04,
               flexGrow: 1,
               // textAlign: 'left',
-              textAlign: 'justify',
+              textAlign: "justify",
               letterSpacing: 3.3,
             }}
             // allowFontScaling={true}
@@ -124,10 +132,69 @@ const MushafScreen = () => {
       );
     });
 
+  const newRenderAyat = (ayat) =>
+    ayat.map((ayah) => {
+      return (
+        <>
+          {/* {<AdjustLabel text={ayah.txt} numberOfLines={1} fontSize={21} />} */}
+          {ayah.words.length > 0 &&
+            ayah.words.map((word, index) => {
+              return (
+                <>
+                  <Text
+                    style={{
+                      // backgroundColor: "#ff0",
+                      fontSize: width * 0.043,
+                      flexGrow: 1,
+                      textAlign: "center",
+                      // textAlign: "justify",
+                      // letterSpacing: 3.3,
+                    }}
+                    // allowFontScaling={true}
+                    // minimumFontScale={2.2}
+                    // adjustsFontSizeToFit={true}
+                  >
+                    {word}
+                  </Text>
+                  {/* {index + 1 != ayah.words.length && (
+                    <Text
+                      style={{
+                        // backgroundColor: "#ff0",
+                        fontSize: width * 0.09,
+                        // flexGrow: 1,
+                        // textAlign: 'left',
+                        // textAlign: "justify",
+                        // letterSpacing: 3.3,
+                      }}
+                      // allowFontScaling={true}
+                      // minimumFontScale={2.2}
+                      adjustsFontSizeToFit={true}
+                    >
+                      {" "}
+                    </Text>
+                  )} */}
+                </>
+              );
+            })}
+          {ayah.num && (
+            <Center
+              style={{
+                width: 30,
+                // backgroundColor: "red",
+                flexGrow: 1,
+              }}
+            >
+              {svgLoader.getSurahNumBorder(ayah.num, 26)}
+            </Center>
+          )}
+        </>
+      );
+    });
+
   const renderPart = () => {
     // console.log('123');
     const replaceNum = (chars) => {
-      const numbers = ['۰', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+      const numbers = ["۰", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
 
       for (let i = 0; i < chars.length; i++) {
         if (numbers.includes(chars[i])) {
@@ -138,13 +205,13 @@ const MushafScreen = () => {
 
       return chars;
     };
-    let string = '';
+    let string = "";
     allQuranLines.slice(16, 31).map((line) => (string += line.lineTxt));
 
     // const newStr = replaceNum(string);
-    let x = string.replace('٦', 'SS');
+    let x = string.replace("٦", "SS");
 
-    string = string.replace('٦', <Doaa />);
+    string = string.replace("٦", <Doaa />);
 
     return <Text style={{ fontSize: 17 }}>{string}</Text>;
   };
@@ -152,13 +219,13 @@ const MushafScreen = () => {
   const renderPageContent = () => {
     return quranReader.getPage(106)?.map((page) => {
       switch (page.type) {
-        case 'Basmalah':
+        case "Basmalah":
           return Basmalah();
 
-        case 'Surah':
+        case "Surah":
           return renderSurahHeader(page.lineTxt);
 
-        case 'Ayah':
+        case "Ayah":
           // return (
           //   <View
           //     style={{
@@ -171,17 +238,33 @@ const MushafScreen = () => {
           //   </View>
           // );
 
+          // return (
+          //   <View
+          //     key={Math.random().toString()}
+          //     style={{
+          //       flexDirection: 'row',
+          //       // justifyContent: 'flex-start',
+          //       // width: '100%',
+          //       backgroundColor: 'green',
+          //     }}
+          //   >
+          //     {renderAyat(page.allAyat)}
+          //   </View>
+          // );
           return (
             <View
               key={Math.random().toString()}
               style={{
-                flexDirection: 'row',
-                // justifyContent: 'flex-start',
+                flexDirection: "row",
+                justifyContent: "space-between",
+                // alignContent: "center",
+                alignItems: "center",
                 // width: '100%',
-                backgroundColor: 'green',
+                // backgroundColor: "green",
+                // height: 40,
               }}
             >
-              {renderAyat(page.allAyat)}
+              {newRenderAyat(page.allAyat)}
             </View>
           );
 
@@ -198,7 +281,7 @@ const MushafScreen = () => {
   // );
 
   return (
-    <Screen style={{}}>
+    <Screen style={{ height: "100%", justifyContent: "space-between" }}>
       {/* {renderPart()} */}
       {renderPageContent()}
       {/* <TestComp /> */}
@@ -210,13 +293,13 @@ export default MushafScreen;
 
 const styles = StyleSheet.create({
   surahHeader: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     bottom: 0,
     left: 0,
-    width: '100%',
-    backgroundColor: 'transparent',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
