c947bb966629274245bc24581b9aec7b76226cf2
kareem aly <kareemaly@kareems-MacBook-Pro.local>
2022-04-02 12:38:36 +0300

    local notification and get push notification kew

--- app/js/screens/ScreenWelcome.js
+++ app/js/screens/ScreenWelcome.js
@@ -1,49 +1,90 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
   View,
   TouchableHighlight,
   ImageBackground,
-} from 'react-native';
-import CheckBox from 'react-native-check-box';
-import { connect } from 'react-redux';
-import { reduxActionSetWelcomeFlag } from '../redux/reduxActions';
-import * as strings from '../helpers/StringsManager';
-import StringsManager from '../helpers/StringsManager';
-import ActionBtn from 'app/components/ActionBtn';
-import Screen from 'app/components/Screen';
+} from "react-native";
+import CheckBox from "react-native-check-box";
+import { connect } from "react-redux";
+import { reduxActionSetWelcomeFlag } from "../redux/reduxActions";
+import * as strings from "../helpers/StringsManager";
+import StringsManager from "../helpers/StringsManager";
+import ActionBtn from "app/components/ActionBtn";
+import Screen from "app/components/Screen";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
 
 class ScreenWelcome extends Component {
   constructor(props) {
     super(props);
     this.stringsManager = new StringsManager();
     this.stringsManager.setLanguage(this.props.strLang);
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+      }),
+    });
+    this.registerForPushNotificationsAsync();
     //console.log(Dimensions.get("window"));
   }
 
-  okButtonPressed() {
-    this.props.navigation.navigate('Home', { screen: 'ScrList' });
+  async okButtonPressed() {
+    await this.schedulePushNotification();
+    // this.props.navigation.navigate("Home", { screen: "ScrList" });
   }
+
+  schedulePushNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: "Here is the notification body",
+      },
+      trigger: { seconds: 1 },
+    });
+  };
+
+  registerForPushNotificationsAsync = async () => {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      // alert('Must use physical device for Push Notifications');
+    }
+    return token;
+  };
 
   render() {
     return (
       <Screen>
         <View style={styles.mainContainer}>
-          <View style={{ width: '100%', alignItems: 'center', height: '68%' }}>
+          <View style={{ width: "100%", alignItems: "center", height: "68%" }}>
             <View style={styles.messageContainer}>
               <ImageBackground
-                source={require('assets/backgroundPNG/green_background_withQuran.png')}
+                source={require("assets/backgroundPNG/green_background_withQuran.png")}
                 style={styles.backgroundImage}
               >
-                <View style={{ width: '93%' }}>
+                <View style={{ width: "93%" }}>
                   <Text style={this.getTitleStyle()}>
                     {this.stringsManager.getStr(strings.STR_GREETING)}
                   </Text>
                   <Text style={this.getSubTitleStyle()}>
                     {this.stringsManager.getStr(strings.STR_MOTIVATION)}
                   </Text>
-                  <View style={[styles.separator, { alignSelf: 'center' }]} />
+                  <View style={[styles.separator, { alignSelf: "center" }]} />
                   <Text style={this.getInstructionsTitleStyle()}>
                     {this.stringsManager.getStr(strings.STR_INSTRUCTIONS_TITLE)}
                   </Text>
@@ -82,7 +123,7 @@ class ScreenWelcome extends Component {
             text={this.stringsManager.getStr(strings.STR_START_NOW)}
             handler={this.okButtonPressed.bind(this)}
             lang={this.props.strLang}
-            style={{ height: 60, width: '100%', marginBottom: '2%' }}
+            style={{ height: 60, width: "100%", marginBottom: "2%" }}
           />
         </View>
       </Screen>
@@ -90,58 +131,58 @@ class ScreenWelcome extends Component {
   }
   getTitleStyle() {
     return {
-      fontSize: this.props.strLang == 'ar' ? 36 : 32,
-      lineHeight: this.props.strLang == 'ar' ? 63 : 50,
-      fontFamily: this.props.strLang == 'ar' ? 'Amiri_Bold' : 'Poppins-Bold',
-      textAlign: 'center',
-      color: '#FFFFFF',
+      fontSize: this.props.strLang == "ar" ? 36 : 32,
+      lineHeight: this.props.strLang == "ar" ? 63 : 50,
+      fontFamily: this.props.strLang == "ar" ? "Amiri_Bold" : "Poppins-Bold",
+      textAlign: "center",
+      color: "#FFFFFF",
       margin: 15,
     };
   }
   getSubTitleStyle() {
     return {
-      fontSize: this.props.strLang == 'ar' ? 22 : 16,
-      lineHeight: this.props.strLang == 'ar' ? 36 : 28,
-      fontFamily: this.props.strLang == 'ar' ? 'Amiri' : 'Poppins',
-      textAlign: 'center',
-      color: '#FFFFFF',
+      fontSize: this.props.strLang == "ar" ? 22 : 16,
+      lineHeight: this.props.strLang == "ar" ? 36 : 28,
+      fontFamily: this.props.strLang == "ar" ? "Amiri" : "Poppins",
+      textAlign: "center",
+      color: "#FFFFFF",
     };
   }
   getInstructionsTitleStyle() {
     return {
-      fontSize: this.props.strLang == 'ar' ? 24 : 18,
-      lineHeight: this.props.strLang == 'ar' ? 36 : 30,
-      fontFamily: this.props.strLang == 'ar' ? 'Amiri_Bold' : 'Poppins-Bold',
-      textDecorationLine: 'underline',
-      color: '#FFFFFF',
+      fontSize: this.props.strLang == "ar" ? 24 : 18,
+      lineHeight: this.props.strLang == "ar" ? 36 : 30,
+      fontFamily: this.props.strLang == "ar" ? "Amiri_Bold" : "Poppins-Bold",
+      textDecorationLine: "underline",
+      color: "#FFFFFF",
 
-      alignSelf: 'flex-start',
+      alignSelf: "flex-start",
       marginBottom: 10,
     };
   }
   getInstructionsStyle() {
     return {
-      fontSize: this.props.strLang == 'ar' ? 22 : 16,
-      lineHeight: this.props.strLang == 'ar' ? 36 : 30,
-      fontFamily: this.props.strLang == 'ar' ? 'Amiri' : 'Poppins',
-      alignSelf: 'flex-start',
-      color: '#FFFFFF',
+      fontSize: this.props.strLang == "ar" ? 22 : 16,
+      lineHeight: this.props.strLang == "ar" ? 36 : 30,
+      fontFamily: this.props.strLang == "ar" ? "Amiri" : "Poppins",
+      alignSelf: "flex-start",
+      color: "#FFFFFF",
     };
   }
   getCheckBoxContainerStyle() {
     return {
-      flexDirection: 'row',
-      width: '100%',
-      marginTop: '2%',
+      flexDirection: "row",
+      width: "100%",
+      marginTop: "2%",
     };
   }
   getCheckBoxTextStyle() {
     return {
-      fontSize: this.props.strLang == 'ar' ? 15 : 13,
-      fontFamily: this.props.strLang == 'ar' ? 'Amiri_Bold' : 'Poppins-Bold',
-      textAlign: 'center',
-      color: '#0C3D11',
-      lineHeight: this.props.strLang == 'ar' ? 30 : 28,
+      fontSize: this.props.strLang == "ar" ? 15 : 13,
+      fontFamily: this.props.strLang == "ar" ? "Amiri_Bold" : "Poppins-Bold",
+      textAlign: "center",
+      color: "#0C3D11",
+      lineHeight: this.props.strLang == "ar" ? 30 : 28,
       marginHorizontal: 4,
     };
   }
@@ -159,27 +200,27 @@ export default connect(mapStateToProps, mapDispatchToProps())(ScreenWelcome);
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#EEEEEE',
-    justifyContent: 'space-between',
+    alignItems: "center",
+    backgroundColor: "#EEEEEE",
+    justifyContent: "space-between",
   },
   messageContainer: {
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-    justifyContent: 'center',
-    width: '100%',
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    justifyContent: "center",
+    width: "100%",
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   backgroundImage: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
 
   separator: {
-    borderColor: '#FFFFFF59',
+    borderColor: "#FFFFFF59",
     borderWidth: 1,
     borderRadius: 10,
     width: 280,
